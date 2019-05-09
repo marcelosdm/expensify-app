5f62ad851e60761dccef2660cad3e55cda283207
marcelosdm <marcelosdm@gmail.com>
2019-05-09 08:08:42 -0300

    Add snapshot testing

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ExpenseListItem = (
+export const ExpenseListItem = (
   { id, description, amount, createdAt } = props.expenses
 ) => (
   <div>
